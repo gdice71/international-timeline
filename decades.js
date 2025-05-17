b0ea8f30e304e9eb24011e7a995fc09543ba436b
Grace Dice <gracedice@Graces-MacBook-Air.local>
2025-05-17 09:16:46 -0400

    “changes“

--- decades.js
+++ decades.js
@@ -1,14 +1,14 @@
-import data1640s from './years/data-1640s.js';
-import data1800s from './years/data-1800s.js';
-import data1810s from './years/data-1810s.js';
-import data1820s from './years/data-1820s.js';
-import data1910s from './years/data-1910s.js';
-import data1940s from './years/data-1940s.js';
-import data1960s from './years/data-1960s.js';
-import data1970s from './years/data-1970s.js';
-import data1980s from './years/data-1980s.js';
-import data2000s from './years/data-2000s.js';
-import data2010s from './years/data-2010s.js';
+import { timelineData as data1640s } from './years/data-1640s.js';
+import { timelineData as data1800s } from './years/data-1800s.js';
+import { timelineData as data1810s } from './years/data-1810s.js';
+import { timelineData as data1820s } from './years/data-1820s.js';
+import { timelineData as data1910s } from './years/data-1910s.js';
+import { timelineData as data1940s } from './years/data-1940s.js';
+import { timelineData as data1960s } from './years/data-1960s.js';
+import { timelineData as data1970s } from './years/data-1970s.js';
+import { timelineData as data1980s } from './years/data-1980s.js';
+import { timelineData as data2000s } from './years/data-2000s.js';
+import { timelineData as data2010s } from './years/data-2010s.js';
 
 export const decades = [
   { decade: '1640s', data: data1640s },
