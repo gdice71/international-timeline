8a8fd2d80be86bb02d57b0df70d5b719eeafe340
Grace Dice <gracedice@Graces-MacBook-Air.local>
2025-05-17 09:39:13 -0400

    “changes“

--- years/data-1800s.js
+++ years/data-1800s.js
@@ -1,41 +1,9 @@
 export const timelineData = {
-    title: {
-      text: {
-        headline: 'International Relations Timeline',
-        text: 'Key events shaping global diplomacy, conflicts, and treaties.'
-      }
-    },
-    events: 
-    [
-      {
-        media: { url: "https://upload.wikimedia.org/wikipedia/commons/5/58/EnterpriseTripoli.jpg" },
-        start_date: 
-        {
-            year: "1801",
-            month: "6", 
-            day: "10"   
-        },
-        text: {
-          headline: 'First Barbary War Begins',
-          text: 'The United States initiates military operations against Tripoli to combat piracy and protect American merchant ships.'
-        },
-        group: 'Africa',
-        category: 'Conflict'
-      },
-      {
-        media: { url: "https://bostonraremaps.com/wp-content/uploads/2017/04/BRM2745-Wilkinson-North-America-1804.jpg" },
-        start_date: 
-        {
-            year: "1803",
-            month: "4", 
-            day: "30"   
-        },
-        text: {
-          headline: 'Louisiana Purchase',
-          text: 'The United States acquires the Louisiana Territory from France, doubling its size and expanding westward.'
-        },
-        group: 'North America',
-        category: 'Treaty'
-      }
-    ]
-  };
+  title: {
+    text: {
+      headline: "",
+      text: ""
+    }
+  },
+  events: []
+};
---
+++ years/data-1930s.js
@@ -0,0 +1,26 @@
+export const timelineData = {
+    title: {
+      text: {
+        headline: "",
+        text: ""
+      }
+    },
+    events: 
+    [
+      {
+        media: { url: "" },
+        start_date: 
+        {
+            year: "",
+            month: "", 
+            day: ""   
+        },
+        text: {
+          headline: "",
+          text: ""
+        },
+        group: '',
+        category: ''
+      },
+    ]
+  };
