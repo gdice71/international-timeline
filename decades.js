c1c613c9701a93f71ff2da3709b66e022e0263d5
Grace Dice <gracedice@Graces-MacBook-Air.local>
2025-05-17 09:51:21 -0400

    “changes“

--- decades.js
+++ decades.js
@@ -1,25 +1,139 @@
+import { timelineData as data1400s } from './years/data-1400s.js';
+import { timelineData as data1410s } from './years/data-1410s.js';
+import { timelineData as data1420s } from './years/data-1420s.js';
+import { timelineData as data1430s } from './years/data-1430s.js';
+import { timelineData as data1440s } from './years/data-1440s.js';
+import { timelineData as data1450s } from './years/data-1450s.js';
+import { timelineData as data1460s } from './years/data-1460s.js';
+import { timelineData as data1470s } from './years/data-1470s.js';
+import { timelineData as data1480s } from './years/data-1480s.js';
+import { timelineData as data1490s } from './years/data-1490s.js';
+
+import { timelineData as data1500s } from './years/data-1500s.js';
+import { timelineData as data1510s } from './years/data-1510s.js';
+import { timelineData as data1520s } from './years/data-1520s.js';
+import { timelineData as data1530s } from './years/data-1530s.js';
+import { timelineData as data1540s } from './years/data-1540s.js';
+import { timelineData as data1550s } from './years/data-1550s.js';
+import { timelineData as data1560s } from './years/data-1560s.js';
+import { timelineData as data1570s } from './years/data-1570s.js';
+import { timelineData as data1580s } from './years/data-1580s.js';
+import { timelineData as data1590s } from './years/data-1590s.js';
+
+import { timelineData as data1600s } from './years/data-1600s.js';
+import { timelineData as data1610s } from './years/data-1610s.js';
+import { timelineData as data1620s } from './years/data-1620s.js';
+import { timelineData as data1630s } from './years/data-1630s.js';
 import { timelineData as data1640s } from './years/data-1640s.js';
+import { timelineData as data1650s } from './years/data-1650s.js';
+import { timelineData as data1660s } from './years/data-1660s.js';
+import { timelineData as data1670s } from './years/data-1670s.js';
+import { timelineData as data1680s } from './years/data-1680s.js';
+import { timelineData as data1690s } from './years/data-1690s.js';
+
+import { timelineData as data1700s } from './years/data-1700s.js';
+import { timelineData as data1710s } from './years/data-1710s.js';
+import { timelineData as data1720s } from './years/data-1720s.js';
+import { timelineData as data1730s } from './years/data-1730s.js';
+import { timelineData as data1740s } from './years/data-1740s.js';
+import { timelineData as data1750s } from './years/data-1750s.js';
+import { timelineData as data1760s } from './years/data-1760s.js';
+import { timelineData as data1770s } from './years/data-1770s.js';
+import { timelineData as data1780s } from './years/data-1780s.js';
+import { timelineData as data1790s } from './years/data-1790s.js';
+
 import { timelineData as data1800s } from './years/data-1800s.js';
 import { timelineData as data1810s } from './years/data-1810s.js';
 import { timelineData as data1820s } from './years/data-1820s.js';
+import { timelineData as data1830s } from './years/data-1830s.js';
+import { timelineData as data1840s } from './years/data-1840s.js';
+import { timelineData as data1850s } from './years/data-1850s.js';
+import { timelineData as data1860s } from './years/data-1860s.js';
+import { timelineData as data1870s } from './years/data-1870s.js';
+import { timelineData as data1880s } from './years/data-1880s.js';
+import { timelineData as data1890s } from './years/data-1890s.js';
+
+import { timelineData as data1900s } from './years/data-1900s.js';
 import { timelineData as data1910s } from './years/data-1910s.js';
+import { timelineData as data1920s } from './years/data-1920s.js';
+import { timelineData as data1930s } from './years/data-1930s.js';
 import { timelineData as data1940s } from './years/data-1940s.js';
+import { timelineData as data1950s } from './years/data-1950s.js';
 import { timelineData as data1960s } from './years/data-1960s.js';
 import { timelineData as data1970s } from './years/data-1970s.js';
 import { timelineData as data1980s } from './years/data-1980s.js';
+import { timelineData as data1990s } from './years/data-1990s.js';
+
 import { timelineData as data2000s } from './years/data-2000s.js';
 import { timelineData as data2010s } from './years/data-2010s.js';
 
 export const decades = [
+  { decade: '1400s', data: data1400s },
+  { decade: '1410s', data: data1410s },
+  { decade: '1420s', data: data1420s },
+  { decade: '1430s', data: data1430s },
+  { decade: '1440s', data: data1440s },
+  { decade: '1450s', data: data1450s },
+  { decade: '1460s', data: data1460s },
+  { decade: '1470s', data: data1470s },
+  { decade: '1480s', data: data1480s },
+  { decade: '1490s', data: data1490s },
+
+  { decade: '1500s', data: data1500s },
+  { decade: '1510s', data: data1510s },
+  { decade: '1520s', data: data1520s },
+  { decade: '1530s', data: data1530s },
+  { decade: '1540s', data: data1540s },
+  { decade: '1550s', data: data1550s },
+  { decade: '1560s', data: data1560s },
+  { decade: '1570s', data: data1570s },
+  { decade: '1580s', data: data1580s },
+  { decade: '1590s', data: data1590s },
+
+  { decade: '1600s', data: data1600s },
+  { decade: '1610s', data: data1610s },
+  { decade: '1620s', data: data1620s },
+  { decade: '1630s', data: data1630s },
   { decade: '1640s', data: data1640s },
+  { decade: '1650s', data: data1650s },
+  { decade: '1660s', data: data1660s },
+  { decade: '1670s', data: data1670s },
+  { decade: '1680s', data: data1680s },
+  { decade: '1690s', data: data1690s },
+
+  { decade: '1700s', data: data1700s },
+  { decade: '1710s', data: data1710s },
+  { decade: '1720s', data: data1720s },
+  { decade: '1730s', data: data1730s },
+  { decade: '1740s', data: data1740s },
+  { decade: '1750s', data: data1750s },
+  { decade: '1760s', data: data1760s },
+  { decade: '1770s', data: data1770s },
+  { decade: '1780s', data: data1780s },
+  { decade: '1790s', data: data1790s },
+
   { decade: '1800s', data: data1800s },
   { decade: '1810s', data: data1810s },
   { decade: '1820s', data: data1820s },
+  { decade: '1830s', data: data1830s },
+  { decade: '1840s', data: data1840s },
+  { decade: '1850s', data: data1850s },
+  { decade: '1860s', data: data1860s },
+  { decade: '1870s', data: data1870s },
+  { decade: '1880s', data: data1880s },
+  { decade: '1890s', data: data1890s },
+
+  { decade: '1900s', data: data1900s },
   { decade: '1910s', data: data1910s },
+  { decade: '1920s', data: data1920s },
+  { decade: '1930s', data: data1930s },
   { decade: '1940s', data: data1940s },
+  { decade: '1950s', data: data1950s },
   { decade: '1960s', data: data1960s },
   { decade: '1970s', data: data1970s },
   { decade: '1980s', data: data1980s },
+  { decade: '1990s', data: data1990s },
+
   { decade: '2000s', data: data2000s },
   { decade: '2010s', data: data2010s },
 ];
