3660883d66fb1019c306dcc4ae1c1dddbaabd377
Grace Dice <gracedice@Graces-MacBook-Air.local>
2025-05-17 09:45:45 -0400

    “changes“

--- decades.js
+++ decades.js
@@ -1,25 +1,24 @@
-import { timelineData as data1640s } from './years/data-1640s.js';
-import { timelineData as data1800s } from './years/data-1800s.js';
-import { timelineData as data1810s } from './years/data-1810s.js';
-import { timelineData as data1820s } from './years/data-1820s.js';
-import { timelineData as data1910s } from './years/data-1910s.js';
-import { timelineData as data1940s } from './years/data-1940s.js';
-import { timelineData as data1960s } from './years/data-1960s.js';
-import { timelineData as data1970s } from './years/data-1970s.js';
-import { timelineData as data1980s } from './years/data-1980s.js';
-import { timelineData as data2000s } from './years/data-2000s.js';
-import { timelineData as data2010s } from './years/data-2010s.js';
-
-export const decades = [
-  { decade: '1640s', data: data1640s },
-  { decade: '1800s', data: data1800s },
-  { decade: '1810s', data: data1810s },
-  { decade: '1820s', data: data1820s },
-  { decade: '1910s', data: data1910s },
-  { decade: '1940s', data: data1940s },
-  { decade: '1960s', data: data1960s },
-  { decade: '1970s', data: data1970s },
-  { decade: '1980s', data: data1980s },
-  { decade: '2000s', data: data2000s },
-  { decade: '2010s', data: data2010s },
+// Define the decades you want to include
+const decadeRanges = [
+  [1400, 1690, 10], // From 1400 to 1690 in steps of 10 (1400s, 1410s, ...)
+  [1700, 1990, 10], // From 1700 to 1990
+  [2000, 2010, 10], // From 2000 to 2010
 ];
+
+// Generate the decade strings dynamically
+const decades = [];
+
+for (const [start, end, step] of decadeRanges) {
+  for (let year = start; year <= end; year += step) {
+    const decade = `${year}s`;
+    try {
+      // Dynamically import the data file
+      const data = require(`./years/data-${decade}.js`).timelineData;
+      decades.push({ decade, data });
+    } catch (error) {
+      console.warn(`Data for ${decade} not found.`);
+    }
+  }
+}
+
+export { decades };
