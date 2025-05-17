c1f7c82bd9e4c26afa3ae157c2c934712204ea59
Grace Dice <gracedice@Graces-MacBook-Air.local>
2025-05-17 09:05:57 -0400

    “changes“

--- decades-config.js
+++ decades-config.js
@@ -1,14 +1,25 @@
-export const decades = 
-[
-    { decade: '1640s', file: './years/data-1640s.js' },
-    { decade: '1800s', file: './years/data-1800s.js' },
-    { decade: '1810s', file: './years/data-1810s.js' },
-    { decade: '1820s', file: './years/data-1820s.js' },
-    { decade: '1910s', file: './years/data-1910s.js' },
-    { decade: '1940s', file: './years/data-1940s.js' },
-    { decade: '1960s', file: './years/data-1960s.js' },
-    { decade: '1970s', file: './years/data-1970s.js' },
-    { decade: '1980s', file: './years/data-1980s.js' },
-    { decade: '2000s', file: './years/data-2000s.js' },
-    { decade: '2010s', file: './years/data-2010s.js' }
-  ];
+import data1640s from './years/data-1640s.js';
+import data1800s from './years/data-1800s.js';
+import data1810s from './years/data-1810s.js';
+import data1820s from './years/data-1820s.js';
+import data1910s from './years/data-1910s.js';
+import data1940s from './years/data-1940s.js';
+import data1960s from './years/data-1960s.js';
+import data1970s from './years/data-1970s.js';
+import data1980s from './years/data-1980s.js';
+import data2000s from './years/data-2000s.js';
+import data2010s from './years/data-2010s.js';
+
+export const decades = [
+  { decade: '1640s', data: data1640s },
+  { decade: '1800s', data: data1800s },
+  { decade: '1810s', data: data1810s },
+  { decade: '1820s', data: data1820s },
+  { decade: '1910s', data: data1910s },
+  { decade: '1940s', data: data1940s },
+  { decade: '1960s', data: data1960s },
+  { decade: '1970s', data: data1970s },
+  { decade: '1980s', data: data1980s },
+  { decade: '2000s', data: data2000s },
+  { decade: '2010s', data: data2010s },
+];
