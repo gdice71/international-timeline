2c3094a8a379ea1fef194b761d9f9ea29575853b
Grace Dice <gracedice@Graces-MacBook-Air.local>
2025-05-19 09:20:04 -0400

    Standardize format of data-2000s.js for parsing

--- years/data-2000s.js
+++ years/data-2000s.js
@@ -1,58 +1,55 @@
-export const timelineData = 
-{
-  title: { text: { headline: 'International Relations Timeline', text: '' } },
-  events: 
-  [
+export const timelineData = {
+  title: { text: { headline: "International Relations Timeline", text: "" } },
+  events: [
     {
-      text: 
-      {
-        headline: 'Russo-Georgian War',
+      text: {
+        headline: "Russo-Georgian War",
         text: "Conflict erupts between Russia and Georgia over the breakaway regions of South Ossetia and Abkhazia, leading to a brief war and subsequent Russian recognition of the regions' independence."
       },
       id: "event-2000-005",
       start_date: {
-        year: '2008',
-        month: '08',
-        day: '08'
+        year: "2008",
+        month: "08",
+        day: "08"
       },
-      group: 'Europe',
-      category: 'Conflict',
+      group: "Europe",
+      category: "Conflict",
       media: {
-        url: 'https://media.cnn.com/api/v1/images/stellar/prod/140106165256-georgia-conflict-2008.jpg?q=w_3000,h_1996,x_0,y_0,c_fill'
+        url: "https://media.cnn.com/api/v1/images/stellar/prod/140106165256-georgia-conflict-2008.jpg?q=w_3000,h_1996,x_0,y_0,c_fill"
       }
     },
     {
       text: {
-        headline: '9/11 Attacks',
-        text: 'Terrorist attacks on the United States lead to the War on Terror and military intervention in Afghanistan.'
+        headline: "9/11 Attacks",
+        text: "Terrorist attacks on the United States lead to the War on Terror and military intervention in Afghanistan."
       },
       id: "event-2000-001",
       start_date: {
-        year: '2001',
-        month: '09',
-        day: '11'
+        year: "2001",
+        month: "09",
+        day: "11"
       },
-      group: 'North America',
-      category: 'Terrorism',
+      group: "North America",
+      category: "Terrorism",
       media: {
-        url: 'https://cdn.britannica.com/15/152315-050-226AA671/twin-towers-skyline-Lower-Manhattan-World-Trade-center.jpg'
+        url: "https://cdn.britannica.com/15/152315-050-226AA671/twin-towers-skyline-Lower-Manhattan-World-Trade-center.jpg"
       }
     },
     {
       text: {
-        headline: 'Iraq War Begins',
-        text: 'The United States, along with coalition forces, invades Iraq to topple Saddam Hussein.'
+        headline: "Iraq War Begins",
+        text: "The United States, along with coalition forces, invades Iraq to topple Saddam Hussein."
       },
       id: "event-2000-002",
       start_date: {
-        year: '2003',
-        month: '03',
-        day: '20'
+        year: "2003",
+        month: "03",
+        day: "20"
       },
-      group: 'Middle East',
-      category: 'Conflict',
+      group: "Middle East",
+      category: "Conflict",
       media: {
-        url: 'https://www.americanspecialops.com/images/force-recon/marines-hmmwvs.jpg'
+        url: "https://www.americanspecialops.com/images/force-recon/marines-hmmwvs.jpg"
       },
       primarySources: [
         "https://archive.globalpolicy.org/component/content/article/167-attack/35266-president-george-w-bushs-speech-on-iraq.html",
@@ -64,71 +61,71 @@ export const timelineData =
     },
     {
       text: {
-        headline: 'EU Expansion',
-        text: 'Ten new countries join the European Union, marking its largest single expansion.'
+        headline: "EU Expansion",
+        text: "Ten new countries join the European Union, marking its largest single expansion."
       },
       id: "event-2000-003",
       start_date: {
-        year: '2004',
-        month: '05',
-        day: '01'
+        year: "2004",
+        month: "05",
+        day: "01"
       },
-      group: 'Europe',
-      category: 'Integration',
+      group: "Europe",
+      category: "Integration",
       media: {
-        url: 'https://eu.bilgi.edu.tr/media/blogs/10/2.png'
+        url: "https://eu.bilgi.edu.tr/media/blogs/10/2.png"
       }
     },
     {
       text: {
-        headline: 'Kyoto Protocol Comes Into Force',
-        text: 'An international treaty aimed at reducing greenhouse gas emissions becomes legally binding.'
+        headline: "Kyoto Protocol Comes Into Force",
+        text: "An international treaty aimed at reducing greenhouse gas emissions becomes legally binding."
       },
       id: "event-2000-004",
       start_date: {
-        year: '2005',
-        month: '02',
-        day: '16'
+        year: "2005",
+        month: "02",
+        day: "16"
       },
-      group: 'Global',
-      category: 'Environmental Treaty',
+      group: "Global",
+      category: "Environmental Treaty",
       media: {
-        url: 'https://cdn.britannica.com/35/91935-050-C178046B/Vice-Pres-speech-Al-Gore-conference-Kyoto-December-1997.jpg'
+        url: "https://cdn.britannica.com/35/91935-050-C178046B/Vice-Pres-speech-Al-Gore-conference-Kyoto-December-1997.jpg"
       }
     },
     {
       text: {
-        headline: 'Global Financial Crisis',
-        text: 'Economic collapse begins with the bankruptcy of Lehman Brothers, triggering a global recession.'
+        headline: "Global Financial Crisis",
+        text: "Economic collapse begins with the bankruptcy of Lehman Brothers, triggering a global recession."
       },
       id: "event-2000-006",
       start_date: {
-        year: '2008',
-        month: '09',
-        day: '15'
+        year: "2008",
+        month: "09",
+        day: "15"
       },
-      group: 'Global',
-      category: 'Economic Crisis',
+      group: "Global",
+      category: "Economic Crisis",
       media: {
-        url: 'https://media.licdn.com/dms/image/v2/C5112AQHIkkozwMyyow/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1568657254235?e=2147483647&v=beta&t=lne47nKRFXrMHFdy8Psv7VnBbdpcTdiE2MVQRJC0udA'
+        url: "https://media.licdn.com/dms/image/v2/C5112AQHIkkozwMyyow/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1568657254235?e=2147483647&v=beta&t=lne47nKRFXrMHFdy8Psv7VnBbdpcTdiE2MVQRJC0udA"
       }
     },
     {
       text: {
-        headline: 'Treaty of Lisbon Signed',
-        text: 'Reforms the European Union to increase efficiency and streamline decision-making.'
+        headline: "Treaty of Lisbon Signed",
+        text: "Reforms the European Union to increase efficiency and streamline decision-making."
       },
       id: "event-2000-007",
       start_date: {
-        year: '2007',
-        month: '12',
-        day: '13'
+        year: "2007",
+        month: "12",
+        day: "13"
       },
-      group: 'Europe',
-      category: 'Treaty',
+      group: "Europe",
+      category: "Treaty",
       media: {
-        url: 'https://www.cvce.eu/content/publication/2007/12/20/f8ace492-aa08-4dcf-9cb5-79dff98eaaac/publishable.jpg'
+        url: "https://www.cvce.eu/content/publication/2007/12/20/f8ace492-aa08-4dcf-9cb5-79dff98eaaac/publishable.jpg"
       }
     }
-  ] 
+  ]
 };
